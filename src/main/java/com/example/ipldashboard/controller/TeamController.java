ff3f23d76b255878f88ab7bb57c072ce2e5c5bea
Gourav Agarwal <gourav.agarwal023@gmail.com>
2021-05-13 16:37:02 +0530

    added team data APIs

---
+++ src/main/java/com/example/ipldashboard/controller/TeamController.java
@@ -0,0 +1,43 @@
+package com.example.ipldashboard.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ipldashboard.model.Match;
+import com.example.ipldashboard.model.Team;
+import com.example.ipldashboard.repository.MatchRepository;
+import com.example.ipldashboard.repository.TeamRepository;
+
+@RestController
+@CrossOrigin
+@RequestMapping("iplApi/")
+public class TeamController {
+
+	@Autowired
+    private TeamRepository teamRepository;
+	@Autowired
+    private MatchRepository matchRepository;
+
+
+    @GetMapping("/team")
+    public Iterable<Team> getAllTeam() {
+        return this.teamRepository.findAll();
+    }
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName) {
+        Team team = this.teamRepository.findByTeamName(teamName);
+        team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName,4));
+            
+        return team;
+    }
+
+}    
---
+++ src/main/java/com/example/ipldashboard/repository/TeamRepository.java
@@ -0,0 +1,11 @@
+package com.example.ipldashboard.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.ipldashboard.model.Team;
+
+public interface TeamRepository extends CrudRepository<Team, Long>  {
+
+    Team findByTeamName(String teamName);
+    
+}
